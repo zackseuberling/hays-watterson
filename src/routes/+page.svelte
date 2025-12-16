b9a27ae8d969d6e06de33800571615165b87eb19
Zack Seuberling <seuberling@gmail.com>
2025-12-16 10:53:21 -0800

    feat: load after a brief flash of color

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -16,36 +16,31 @@
   }
 
   let colors = ["#932147", "#FF6363", "#873983", "#9CB3C2", "#B57D26", "#4257FA"]
-  let randomColors = shuffleArrayInPlace(colors).splice(0,5);
+  let randomColors = shuffleArrayInPlace(colors);
 
   onMount(() => {
-    // Support Test
-    const supportsContainerQueries = "container" in document.documentElement.style;
-
-    if (!supportsContainerQueries) {
-      // @ts-ignore
-      import("https://cdn.skypack.dev/container-query-polyfill");
-    }
-
     const htmlElement = document.documentElement;
-    const height = htmlElement.offsetHeight;
 
-    htmlElement.style.setProperty("--height", height.toString())
+    htmlElement.classList.add("opacity-100")
     htmlElement.style.setProperty("--color-1", randomColors[0])
     htmlElement.style.setProperty("--color-2", randomColors[1])
     htmlElement.style.setProperty("--color-3", randomColors[2])
     htmlElement.style.setProperty("--color-4", randomColors[3])
     htmlElement.style.setProperty("--color-5", randomColors[4])
+    htmlElement.style.setProperty("--color-6", randomColors[5])
+
+    setTimeout(() => {
+      const height = htmlElement.offsetHeight;
+      htmlElement.style.setProperty("--height", height.toString())
+    }, 25)
   })
 </script>
-
-
 
 <div class="left-side set-height absolute left-0 right-1/2 h-dvh"></div>
 <div class="right-side set-height absolute right-0 left-1/2 h-dvh"></div>
 <div class="fixed inset-y-0 right-1/2 -mr-8 w-16 bg-transparent backdrop-blur-sm"></div>
 
-<article class="relative z-20 max-w-4xl mx-auto p-24 text-2xl text-white">
+<article class="relative z-20 max-w-4xl mx-auto p-24 text-2xl">
   <header class="stuck-top sticky top-0 mb-8">
     <h1 class="text-3xl mb-4">Hays Watterson Memorial Scholarship</h1>
     <img src="/hays-watterson.jpeg" height="300" class="aspect-5/3 object-cover block mx-auto" alt="" />
@@ -56,7 +51,7 @@
 <button class="sticky top-54 button p-4 bg-amber-50 mx-auto block rounded-full my-8">Donate to the Hays Watterson Memorial Scholarship Fund</button>
 
 <section>
-  <h2 class="my-8">About Alexandra and Brian</h2>
+  <h2 class="text-center text-xl mt-8 mb-4">About Alexandra and Brian</h2>
 
   <p>Alexandra Hays and Brian Watterson met as graphic design MFA students at Yale, became life partners, and went on to flourishing design careers in the technology sector.</p>
   <p>Alexandra Hays (1984-2024) attended the Yale School of Art in 2010. Her artistic sensibility and special attention to the small joys of daily life enriched her professional activities that included gallery director, designer, and writer. Early in her career, she worked as an artist, asking fundamental questions about who is an artist, and what is art for. She later went on to work at Gladstone Gallery, then Google, where she excelled as a writer and project director.</p>
@@ -65,7 +60,7 @@
 </section>
 
 <section>
-  <h2 class="my-8">About the Scholarship</h2>
+  <h2 class="text-center text-xl mt-8 mb-4">About the Scholarship</h2>
 
   <p>The Hays Watterson Memorial Scholarship is awarded annually to a second-year Yale MFA student in the graphic design department in memory of Alexandra Hays and Brian Watterson. This scholarship recognizes a talented designer who approaches technology with the kind of imagination and humanism that Alex and Brian brought to their life and work. The recipient is selected by a panel of full time faculty.</p>
   <p>This award is supported through a gift made by Alexandra and Brian’s families, friends, and colleagues. As an endowed fund, this scholarship will be awarded in perpetuity, with the potential over time to provide full tuition for a second-year graphic design student.</p>
