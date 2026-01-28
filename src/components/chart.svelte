126058d8892c61404d990e2ab4d6f59bc82577cd
Zack Seuberling <seuberling@gmail.com>
2026-01-28 10:29:46 -0800

    fix: bg colors

--- src/components/chart.svelte
+++ src/components/chart.svelte
@@ -4,19 +4,19 @@
       Fundraising Goal <br />
       <em>June 1, 2026</em>
     </div>
-    <div class="border-t p-2 border-white row-span-5 col-span-2 bg-white/30">
+    <div class="border-t p-2 border-white row-span-5 col-span-2 bg-white/15">
       $100,000
     </div>
     <div class="border-t p-2 border-white row-span-2 font-sans text-sm">
       Fund Secured
     </div>
-    <div class="border-t p-2 border-white row-span-2 col-span-2 bg-white/50">
+    <div class="border-t p-2 border-white row-span-2 col-span-2 bg-white/40">
       $50,000
     </div>
     <div class="border-t p-2 border-white row-span-3 font-sans text-sm">
       Donations to date
     </div>
-    <div class="border-t p-2 border-white row-span-3 col-span-2 bg-white/60">
+    <div class="border-t p-2 border-white row-span-3 col-span-2 bg-white/80">
       $35,000
     </div>
   </div>
